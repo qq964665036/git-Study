2e784fef765b538b15ef012ddc734fe1fe5b8e2b
likailuo <964665036@qq.com>
2023-04-05 17:12:08 +0800

    第一次修改

---
+++ src/main/java/com/zydwz/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.zydwz.pojo;
+
+import lombok.Data;
+
+/**
+ * @author 章鱼大丸子
+ */
+@Data
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/zydwz/pojo/Teacher.java
@@ -0,0 +1,10 @@
+package com.zydwz.pojo;
+
+import lombok.Data;
+
+@Data
+public class Teacher {
+    private String id;
+    private String name;
+    private int age;
+}
